88caac3cc77324d45900d8b7f228b6b667fecd67
perlaagomezz <gomezperla879@gmail.com>
2021-10-19 09:53:04 -0700

    I created a html,css,javascript.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+console.log('My script is working')
+
+document.write("<h1>This is my JavaScript Demo.</h1>")
+document.write("<h3>This is the h2 in my JavaScript Demo.</h3> ")
+
+alert('This is a alert on the my main page.')
+
+alert('here is the second one.')
+
+alert('are you mad yet?')
+
+alert('OKAY IM DONEE!')
+
+alert('SIKKKEEE!')
+prompt('Do you like these?')
+prompt('why dont you like them?')
+
